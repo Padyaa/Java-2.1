866dca9d8b62cffa74ef0f29bd4a8f21646dcd5d
Padyaa <zehopa@mail.ru>
2022-06-01 11:39:10 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 14600;
+        int milse = 500;
+        int bonus = price / milse;
+        System.out.println(bonus);
+    }
+}
